94d93d17a7b57828278d96576de872eb9930a1c8
Daniil <116579474+UnexarT@users.noreply.github.com>
2023-04-24 01:35:43 +0500

    Update zad_9.java

--- zad_9.java
+++ zad_9.java
@@ -4,7 +4,7 @@ public class zad_9 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         System.out.println("Введите текст: ");
-        String text = scn.next();
+        String text = scn.nextLine();
         String replace = Replace_Coms(text);
         if (replace.equals(text)){
             System.out.println("Комментарий в тексте нет");
@@ -14,9 +14,9 @@ public class zad_9 {
     }
 
     public static String Replace_Coms(String text) {
-        String replace = text.replaceAll("[-].+[-]","");
-        replace = replace.replaceAll("[_].+[_]","");
-        replace = replace.replaceAll("[=].+[=]","");
+        String replace = text.replaceAll("-.+-","");
+        replace = replace.replaceAll("_.+_","");
+        replace = replace.replaceAll("=.+=","");
         return replace;
     }
 
